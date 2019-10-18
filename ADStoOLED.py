58f6983f1d492c87dd2c5dcf73b7be7a7f9ce1f3
einstein371 <36686741+einstein371@users.noreply.github.com>
2019-10-18 16:35:00 -0400

    Add files via upload
    Python code to read from the ADC (ADS1115) and display the value on an OLED.

---
+++ ADStoOLED.py
@@ -0,0 +1,144 @@
+# Copyright (c) 2017 Adafruit Industries
+# Author: Tony DiCola & James DeVito
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+import Adafruit_ADS1x15
+#from simpletest.py import values
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+adc = Adafruit_ADS1x15.ADS1115()
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+#DC = 23
+#SPI_PORT = 0
+#SPI_DEVICE = 0
+global values
+GAIN = 2/3
+# Beaglebone Black pin configuration:
+# RST = 'P9_12'
+# Note the following are only used with SPI:
+# DC = 'P9_15'
+# SPI_PORT = 1
+# SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+#disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Note you can change the I2C address by passing an i2c_address parameter like:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+
+# Alternatively you can specify an explicit I2C bus number, for example
+# with the 128x32 display you would use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+
+# 128x32 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# 128x64 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Alternatively you can specify a software SPI implementation by providing
+# digital GPIO pin numbers for all the required display pins.  For example
+# on a Raspberry Pi with the 128x32 display you might use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+while True:
+    # Read all the ADC channel values in a list.
+    values = [0]*4
+    for i in range(4):
+        # Read the specified ADC channel using the previously set gain value.
+     
+        adc.start_adc(0, gain=GAIN)
+        values[i] = adc.get_last_result()
+        # Note you can also pass in an optional data_rate parameter that controls
+        # the ADC conversion time (in samples/second). Each chip has a different
+        # set of allowed data rate values, see datasheet Table 9 config register
+        # DR bit values.
+        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+        # Each value will be a 12 or 16 bit signed integer value depending on the
+        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+    # Draw a black filled box to clear the image.
+    start = time.time()
+    print('Channel 0: {0}'.format(values))
+ 
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Write two lines of text.
+
+    draw.text((x, top),       str(values[0]) + ' ' + str(values[1]),  font=font, fill=255)
+    #draw.text((x, top+8),     str(CPU), font=font, fill=255)
+   # draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
+    #draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(.5)    
+    # Print the ADC values.
+    #print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+    # Pause for half a second.
+    #time.sleep(5)
+
+    
+
